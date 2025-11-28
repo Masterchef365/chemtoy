758c7784161e53575fe2f77fcd1d1e5fc02cf9c8
Masterchef365 <duncan.freeman1@gmail.com>
2025-11-28 03:46:05 -0800

    Ignore ions for now

--- chemtoy-deduct/janaf/import.py
+++ chemtoy-deduct/janaf/import.py
@@ -9,7 +9,8 @@ import re
 from janaf_parse import analyze_formula, ELEMENT_MASSES
 
 #ELEMENTS = "H|He|Li|Be|B|C|N|O|F|Ne|Na|Mg|Al|Si|P|S|Cl|Ar|K|Ca|Sc|Ti|V|Cr|Mn|Fe|Co|Ni|Cu|Zn|Ga|Ge|As|Se|Br|Kr|Rb|Sr|Y|Zr|Nb|Mo|Tc|Ru|Rh|Pd|Ag|Cd|In|Sn|Sb|Te|I|Xe|Cs|Ba|La|Ce|Pr|Nd|Pm|Sm|Eu|Gd|Tb|Dy|Ho|Er|Tm|Yb|Lu|Hf|Ta|W|Re|Os|Ir|Pt|Au|Hg|Tl|Pb|Bi|Po|At|Rn|Fr|Ra|Ac|Th|Pa|U|Np|Pu|Am|Cm|Bk|Cf|Es|Fm|Md|No|Lr|Rf|Db|Sg|Bh|Hs|Mt|Ds|Rg|Cn|Nh|Fl|Mc|Lv|Ts|Og"
-regex=f"(({ELEMENTS})+[0-9]*)+[+-]?$"
+#regex=f"(({ELEMENTS})+[0-9]*)+[+-]?$"
+regex=f"(({ELEMENTS})+[0-9]*)+$"
 filter = pl.col("formula").str.contains(f"^{regex}")
 filter &= pl.col("phase").str.contains('^g|ref')
 
